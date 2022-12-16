3193f1f0babbeca4adcc335ce6671efd068fee15
Antonio <antonio.tabares@outlook.es>
2022-12-16 16:55:57 -0500

    Error handler established, up to catch error in debug stage

--- src/api/skills/controller.js
+++ src/api/skills/controller.js
@@ -1,28 +1,44 @@
 const helper = require('./helper');
 
-const createSkillsController = async (req, res) => {
-  const { data } = req.body;
-  const result = await helper.createSkillsHelper(data);
-  return res.send(result).status(201);
+const createSkillsController = async (req, res, next) => {
+  try {
+    const { data } = req.body;
+    const result = await helper.createSkillsHelper(data);
+    return res.send(result).status(201);
+  } catch (error) {
+    next(error);
+  }
 }
 
-const getSkillsController = async (req, res) => {
-  const { id } = req.params;
-  const result = await helper.getSkillsHelper(id);
-  return res.send(result).status(200);
+const getSkillsController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await helper.getSkillsHelper(id);
+    return res.send(result).status(200);
+  } catch (error) {
+    next(error);
+  }
 }
 
-const updateSkillsController = async (req, res) => {
-  const { id } = req.params;
-  const { data } = req.body;
-  await helper.updateSkillsHelper(id, data);
-  return res.send({ body: 'Done!' }).status(200);
+const updateSkillsController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { data } = req.body;
+    await helper.updateSkillsHelper(id, data);
+    return res.send({ body: 'Done!' }).status(200);
+  } catch (error) {
+    next(error);
+  }
 }
 
-const deleteSkillsController = async (req, res) => {
-  const { id } = req.params;
-  await helper.deleteSkillsHelper(id);
-  return res.send({ body: 'Done!' }).status(200);
+const deleteSkillsController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await helper.deleteSkillsHelper(id);
+    return res.send({ body: 'Done!' }).status(200);
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
--- src/api/tech_skills/controller.js
+++ src/api/tech_skills/controller.js
@@ -1,28 +1,44 @@
 const helper = require('./helper');
 
-const createTechSkillsController = async (req, res) => {
-  const { data } = req.body;
-  const result = await helper.createTechSkillsHelper(data);
-  return res.send(result).status(201);
+const createTechSkillsController = async (req, res, next) => {
+  try {
+    const { data } = req.body;
+    const result = await helper.createTechSkillsHelper(data);
+    return res.send(result).status(201);
+  } catch (error) {
+    next(error);
+  }
 }
 
-const getTechSkillsController = async (req, res) => {
-  const { id } = req.params;
-  const result = await helper.getTechSkillsHelper(id);
-  return res.send(result).status(200);
+const getTechSkillsController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await helper.getTechSkillsHelper(id);
+    return res.send(result).status(200);
+  } catch (error) {
+    next(error);
+  }
 }
 
-const updateTechSkillsController = async (req, res) => {
-  const { id } = req.params;
-  const { data } = req.body;
-  await helper.updateTechSkillsHelper(id, data);
-  return res.send({ body: 'Done!' }).status(200);
+const updateTechSkillsController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { data } = req.body;
+    await helper.updateTechSkillsHelper(id, data);
+    return res.send({ body: 'Done!' }).status(200);
+  } catch (error) {
+    next(error);
+  }
 }
 
-const deleteTechSkillsController = async (req, res) => {
-  const { id } = req.params;
-  await helper.deleteTechSkillsHelper(id);
-  return res.send({ body: 'Done!' }).status(200);
+const deleteTechSkillsController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await helper.deleteTechSkillsHelper(id);
+    return res.send({ body: 'Done!' }).status(200);
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
