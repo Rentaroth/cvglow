1a7f77f4fde9153e2adf1360fdba3b7cfb1307b2
Antonio <antonio.tabares@outlook.es>
2022-12-28 20:38:34 -0500

    Education endpoint joins fixed

--- src/repositories/educationRepository.js
+++ src/repositories/educationRepository.js
@@ -7,25 +7,46 @@ class EducationRepository extends BaseRepository {
 
   async getEdcationWithJoins(id) {
     const { table } = this;
-    const Utilities = await this.db('Utilities')
-      .select('*')
-      .where({ education_id: id });
-    const Aditionals = await this.db('Aditionals')
-      .select('*')
-      .where({ education_id: id });
-
     const result = await this.db(table)
-      .where(table + '.id', id)
+      .innerJoin('Utilities', table + '.id', '=', 'Utilities.education_id')
+      .innerJoin('Aditionals', table + '.id', '=', 'Aditionals.education_id')
       .select('*')
-    result[0].Utilities = Utilities;
-    result[0].Aditionals = Aditionals;
+      .where(table + '.id', id)
+      .options({ nestTables: true })
+      .then((data) => {
+        let final = data[0].Education,
+          Util = [],
+          Adit = [],
+          filter = [],
+          aditFilter = [];
+        data.map((element) => {
+          if (!Util[0]) {
+            Util.push(element.Utilities);
+            filter.push(element.Utilities.id);
+            Adit.push(element.Aditionals);
+            aditFilter.push(element.Aditionals.id);
+          }
+          if (!filter.includes(element.Utilities.id)) {
+            Util.push(element.Utilities);
+            return filter.push(element.Utilities.id);
+          }
+          if (!aditFilter.includes(element.Aditionals.id)) {
+            Adit.push(element.Aditionals);
+            console.log(Adit)
+            return aditFilter.push(element.Aditionals.id);
+          }
+        });
+        final.Utilities = Util;
+        final.Aditionals = Adit;
+        return final;
+      });
     return result;
   }
 
   async deleteEducationWithForeigns(id) {
     await this.db('Utilities').where({ education_id: id }).del();
     await this.db('Aditionals').where({ education_id: id }).del();
-    const result = await this.delete(id)
+    const result = await this.delete(id);
     return result;
   }
 }
