8f0515b8b970e7e9efa97f20b6872ced21b277ba
Antonio <antonio.tabares@outlook.es>
2023-01-09 20:11:47 -0500

    Working version for deploy

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -4,20 +4,13 @@ const { schema } = require('./swagger_doc');
 
 const doc = {
   definition: schema,
-  apis: ['/**/*.js'], // files containing annotations as above
-};
-
-const options = {
-  customCss:
-    ".swagger-ui .topbar { background-color: #fab2ea} .topbar-wrapper img {content:url('../assets/logo.gif'); width:auto; height:30px;}",
-  customSiteTitle: 'API Docs',
-  customfavIcon: '/assets/favicon.ico',
+  apis: ['./**/*/.js'], // files containing annotations as above
 };
 
 const swaggerSpec = swaggerJSDoc(doc);
 
 const swaggerDocs = (app, port) => {
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   console.log(`Documentation of this API avaiable at http://localhost:${port}/docs`);
 }
 
