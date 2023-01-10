733c1c424ca723d0036dbee076c8fd2683338975
Antonio <antonio.tabares@outlook.es>
2023-01-09 19:23:14 -0500

    Swagger modification to correct error in deploy

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -2,15 +2,22 @@ const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const { schema } = require('./swagger_doc');
 
-const options = {
+const doc = {
   definition: schema,
-  apis: ['./schemas/**/*.schema.js'], // files containing annotations as above
+  apis: ['/**/*.js'], // files containing annotations as above
 };
 
-const swaggerSpec = swaggerJSDoc(options);
+const options = {
+  customCss:
+    ".swagger-ui .topbar { background-color: #fab2ea} .topbar-wrapper img {content:url('../assets/logo.gif'); width:auto; height:30px;}",
+  customSiteTitle: 'API Docs',
+  customfavIcon: '/assets/favicon.ico',
+};
+
+const swaggerSpec = swaggerJSDoc(doc);
 
 const swaggerDocs = (app, port) => {
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
   console.log(`Documentation of this API avaiable at http://localhost:${port}/docs`);
 }
 
