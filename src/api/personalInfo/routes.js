6bf5eabc6336e340efbe6b071db6330ed9188dc0
Antonio <antonio.tabares@outlook.es>
2023-01-15 18:11:50 -0500

    Inserting express.json in put method rute

--- src/api/personalInfo/routes.js
+++ src/api/personalInfo/routes.js
@@ -1,11 +1,12 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const auth = require('../../services/auth').verifyToken;
 const controller = require('./controller');
 
 router.get('/', auth, controller.bringPersonalInfo);
 router.get('/:id', auth, controller.bringPersonalInfo);
 router.post('/', auth, controller.addPersonalInfo);
-router.put('/:id', auth, controller.modifyPersonalInfo);
+router.put('/:id', express.json(), auth, controller.modifyPersonalInfo);
 router.delete('/:id', auth, controller.erasePersonalInfo);
 
 module.exports = router;
