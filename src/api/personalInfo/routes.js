2fe7a2a65bee105aa3ab09ba3b941430a1578891
Antonio <antonio.tabares@outlook.es>
2023-01-15 18:15:20 -0500

    Trying patch method instead of put method

--- src/api/personalInfo/routes.js
+++ src/api/personalInfo/routes.js
@@ -6,7 +6,7 @@ const controller = require('./controller');
 router.get('/', auth, controller.bringPersonalInfo);
 router.get('/:id', auth, controller.bringPersonalInfo);
 router.post('/', auth, controller.addPersonalInfo);
-router.put('/:id', express.json(), auth, controller.modifyPersonalInfo);
+router.patch('/:id', auth, controller.modifyPersonalInfo);
 router.delete('/:id', auth, controller.erasePersonalInfo);
 
 module.exports = router;
